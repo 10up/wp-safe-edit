a458241f3b5c7a94431e2d979ec03c0cf5ffba86
Adam Silverstein <adam@10up.com>
2018-07-06 09:11:30 -0400

    Add mergine/forking support

--- src/index.js
+++ src/index.js
@@ -31,16 +31,59 @@ if ( wp.editPost && 'undefined' !== typeof wp.editPost.PluginSidebarMoreMenuItem
 			console.log( result );
 		}
 
+		async mergeFork( e ) {
+			const id = document.getElementById( 'post_ID' ).value;
+			const request = {
+				path: 'wp-safe-edit/v1/merge/' + id,
+				data: {
+					nonce: gutenbergData.forknonce,
+				},
+				nonce: gutenbergData.forknonce,
+				type: 'GET',
+				dataType: 'json',
+			}
+
+			const result = await apiRequest( request );
+			if ( result.data && result.data.shouldRedirect ) {
+				document.location = result.data.redirectUrl;
+			}
+		}
+
 		componentDidMount() {
+			const { subscribe } = wp.data;
+
+			const initialPostStatus = wp.data.select( 'core/editor' ).getEditedPostAttribute( 'status' );
+
+			if ( 'wpse-draft' === initialPostStatus ) {
+				// Watch for the publish event.
+				const unssubscribe = subscribe( ( e ) => {
+					const currentPostStatus = wp.data.select( 'core/editor' ).getEditedPostAttribute( 'status' );
+					if ( 'publish' === currentPostStatus ) {
+						unssubscribe();
+						console.log('publish');
+						setTimeout( () => {
+							// Merge the fork.
+							this.mergeFork();
+						}, 300 );
+					}
+				} );
+			}
+
+			// Remove any previous notice.
+			wp.data.dispatch( 'core/editor' ).removeNotice( WP_SAFE_EDIT_NOTICE_ID );
+
 			// Display a notice to inform the user if this is a safe draft.
 			var postStatus = wp.data.select( 'core/editor' ).getEditedPostAttribute( 'status' );
 			if ( 'wpse-draft' === postStatus  ) {
 				const message = __( 'A draft has been created and you can edit it below. Publish your changes to make them live.', 'wp-safe-edit' );
-				wp.data.dispatch( 'core/editor' ).createWarningNotice( message, {
-					id: WP_SAFE_EDIT_STATUS_ID,
-				} );
+				wp.data.dispatch( 'core/editor' ).createSuccessNotice(
+					message,
+					{
+						id: WP_SAFE_EDIT_STATUS_ID,
+						isDismissible: false,
+					}
+				);
 			}
-
 		}
 
 		render() {
