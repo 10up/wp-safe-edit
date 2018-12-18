80e5fb429fbca3ccd4bfb43b6708ca59b750c15b
Ephraim G <ephraim.gregor@10up.com>
2018-12-18 13:13:37 -0500

    Updates to work with latest Gutenberg

--- src/index.js
+++ src/index.js
@@ -74,7 +74,7 @@ if ( wp.editPost && 'undefined' !== typeof wp.editPost.PluginSidebarMoreMenuItem
 
 				// Display any message except on for editing page
 				if ( wpSafeEditGutenbergData.message ) {
-					data.dispatch( 'core/editor' ).createSuccessNotice(
+					data.dispatch( 'core/notices' ).createSuccessNotice(
 						element.createElement( 'p', {}, wpSafeEditGutenbergData.message ),
 						{
 							id: WP_SAFE_EDIT_NOTICE_ID,
@@ -82,18 +82,18 @@ if ( wp.editPost && 'undefined' !== typeof wp.editPost.PluginSidebarMoreMenuItem
 					);
 				} else {
 					// Remove any previous notice.
-					data.dispatch( 'core/editor' ).removeNotice( WP_SAFE_EDIT_NOTICE_ID );
+					data.dispatch( 'core/notices' ).removeNotice( WP_SAFE_EDIT_NOTICE_ID );
 				}
 			}
 
 			// Remove any previous notice.
-			data.dispatch( 'core/editor' ).removeNotice( WP_SAFE_EDIT_STATUS_ID );
+			data.dispatch( 'core/notices' ).removeNotice( WP_SAFE_EDIT_STATUS_ID );
 
 			// Display a notice to inform the user if this is a safe draft.
 			var postStatus = data.select( 'core/editor' ).getEditedPostAttribute( 'status' );
 			if ( 'wpse-draft' === postStatus  ) {
 				const message = __( 'A draft has been created and you can edit it below. Publish your changes to make them live.', 'wp-safe-edit' );
-				data.dispatch( 'core/editor' ).createSuccessNotice(
+				data.dispatch( 'core/notices' ).createSuccessNotice(
 					element.createElement( 'p', {}, message ),
 					{
 						id: WP_SAFE_EDIT_STATUS_ID,
